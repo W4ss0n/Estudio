6394ae4a0e3e4332dbf5304e161c88a9bbb3a039
W4ss0n <iamedisonvaca@hotmail.com>
2021-12-12 16:58:31 +0100

    Hola campeón

---
+++ java/a.js
@@ -0,0 +1,22 @@
+let movil = "apple";
+let movilc = "xiaomi";
+let espacio = "-";
+
+let resultado = movil + espacio + movilc;
+
+console.log(resultado)
+
+const paises = {
+    europa : "españa paris francia alemania",
+    latinos : "ecuador colombia peru bolivia",
+    arriba : "canda usa",
+    mejorespaises: {
+            frio : "canada",
+            calor : "españa",
+            templado : "españitadelnorte"
+    }
+}
+
+console.log(paises.mejorespaises.templado);
+
+console.log (paises.mejorespaises.frio +18);
